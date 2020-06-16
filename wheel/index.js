f2732dfe84b1febb32229aad57f4c93bc911429f
shtrih <shtrih@hitagi.ru>
2020-06-16 15:03:56 +0400

    Wheel: Feature: ability to edit lists

--- wheel/index.js
+++ wheel/index.js
@@ -55,12 +55,12 @@ const dataSets = {
         'Комбо неудач',
         'Дух Рэмбо',
         'Настоящий бунтарь',
-        // 'Чуйка на говно',
+        'Чуйка на говно',
         'Не та позиция тебе выпала, стремлер',
-        // 'Выбор Бумера',
-        // 'Выбор Зумера',
-        // 'Чат здесь закон',
-        // 'Я здесь закон',
+        'Выбор Бумера',
+        'Выбор Зумера',
+        'Чат здесь закон',
+        'Я здесь закон',
         'Три топора',
         'Сраное колдунье',
         'Два по цене одного',
@@ -75,7 +75,7 @@ const dataSets = {
         'Сладкий рулет ХПГ',
         'Подземное убежище',
         'Аттракцион невиданной щедрости',
-        // 'Never Lucky',
+        'Never Lucky',
         'Сахарные бомбы',
         'Наелся и спит',
         'Предательский столб',
@@ -117,14 +117,176 @@ const dataSets = {
         'Комбо неудач',
         'Дух Рэмбо',
         'Не та позиция тебе выпала, стремлер',
-        // 'Чуйка на говно',
+        'Чуйка на говно',
         'Три топора',
         'Аттракцион невиданной щедрости',
         'Предательский столб',
         'Падение пиццианской башни',
     ]
 };
-let currentDataSet = 'inventory';
+let currentDataSet = 'inventory',
+    editedDataSets = {};
+
+const editDialog = document.getElementById('dialog-edit'),
+    editButton = document.getElementById('btn-edit'),
+    editConfirmButton = editDialog.getElementsByClassName('apply')[0],
+    editOptions = editDialog.getElementsByClassName('options')[0],
+    editPresets = editDialog.getElementsByClassName('presets')[0],
+    optionClick = function (option, checked) {
+        editedDataSets[currentDataSet][option] = checked;
+    },
+    generateOptions = function (dataObject) {
+        let options = '';
+        for (let i in dataObject) {
+            options += `<label><input type="checkbox" onchange="optionClick('${i}', this.checked)" ${dataObject[i] ? 'checked' : ''} />${i}</label><br />`;
+        }
+
+        return options;
+    },
+    resetEditedDataSet = function () {
+        editedDataSets[currentDataSet] = Object.fromEntries(dataSets[currentDataSet].map(v => v).sort().map(v => [v, true]));
+    },
+    editedDataToArray = function () {
+        let result = [];
+
+        for (let [key, value] of Object.entries(editedDataSets[currentDataSet])) {
+            if (value) {
+                result.push(key)
+            }
+        }
+
+        return result;
+    }
+;
+
+editButton.addEventListener('click', function () {
+    if (currentDataSet === 'custom') {
+        p5Instance.mouseDragEnable(false);
+        customDialog.style.display = 'block';
+
+        return;
+    }
+
+    editDialog.style.display = 'block';
+    p5Instance.mouseDragEnable(false);
+
+    editPresets.innerHTML = '';
+    editPresets.append(...presets.getNodes(currentDataSet));
+    editOptions.innerHTML = generateOptions(editedDataSets[currentDataSet]);
+});
+editConfirmButton.addEventListener('click', function () {
+    editDialog.style.display = 'none';
+    p5Instance.mouseDragEnable();
+
+    p5Instance.setData(editedDataToArray());
+});
+
+class Preset {
+    constructor(title, disabledEntries, isDefault) {
+        this._title = title;
+        this._disabledEntries = disabledEntries;
+        this._isDefault = Boolean(isDefault);
+    }
+
+    get isDefault() {
+        return this._isDefault;
+    }
+
+    get domNode() {
+        const el = document.createElement('a');
+
+        el.setAttribute('href', '#');
+        el.appendChild(document.createTextNode(this._title));
+        el.addEventListener('click', this.handleClick.bind(this));
+
+        return el;
+    }
+
+    handleClick() {
+        resetEditedDataSet();
+
+        for(const i in this._disabledEntries) {
+            if (editedDataSets[currentDataSet][this._disabledEntries[i]]) {
+                editedDataSets[currentDataSet][this._disabledEntries[i]] = false;
+            }
+        }
+
+        editOptions.innerHTML = generateOptions(editedDataSets[currentDataSet]);
+
+        return false;
+    }
+}
+
+class PresetAll extends Preset {
+    constructor(isDefault) {
+        super('Выбрать все', [], isDefault);
+    }
+}
+
+class PresetWithoutSpecialRolls extends Preset {
+    constructor(isDefault) {
+        super(
+            'Без спецроллов',
+            [
+                'Чуйка на говно',
+                'Выбор Бумера',
+                'Выбор Зумера',
+                'Чат здесь закон',
+                'Я здесь закон',
+                'Never Lucky',
+            ],
+            isDefault
+        );
+    }
+}
+
+class Presets {
+    constructor() {
+        this._presets = {
+            // inventory: [
+            //     new PresetAll(),
+            // ],
+            effects: [
+                new PresetAll(),
+                new PresetWithoutSpecialRolls(true),
+            ],
+            debuffs: [
+                new PresetAll(),
+                new PresetWithoutSpecialRolls(true),
+            ],
+            streamers: [
+                new PresetAll(),
+            ],
+        };
+    }
+
+    hasPreset(dataSetKey) {
+        return !!this._presets[dataSetKey];
+    }
+
+    getNodes(dataSetKey) {
+        let result = [];
+
+        for(const i in this._presets[dataSetKey]) {
+            if (i % 2) {
+                result.push(document.createTextNode(', '));
+            }
+            result.push(this._presets[dataSetKey][i].domNode);
+        }
+
+        return result;
+    }
+
+    applyDefaults(dataSetKey) {
+        for(const i in this._presets[dataSetKey]) {
+            if (this._presets[dataSetKey][i].isDefault) {
+                this._presets[dataSetKey][i].handleClick();
+            }
+        }
+    }
+}
+
+const presets = new Presets;
 
 function getImageURI(index) {
     let result = '../hpg-inventory/images/000.png',
@@ -137,21 +299,6 @@ function getImageURI(index) {
             break;
 
         case "effects":
-            // костыли из-за удалённых спецроллов
-            if (index > 23) {
-                offset += 1;
-            }
-            if (index > 24) {
-                offset += 4;
-            }
-            if (index > 38) {
-                offset += 1;
-            }
-            // Чат здесь закон → чат выбирает
-            if (index === 12) {
-                offset += 16;
-            }
-
             result = '../hpg-inventory/images/0' + ('0' + (index+1 + offset)).slice(-2) + '.png';
             break;
 
@@ -162,7 +309,7 @@ function getImageURI(index) {
                 7,
                 10,
                 12,
-                29,
+                13,
                 16,
                 18,
                 20,
@@ -170,7 +317,7 @@ function getImageURI(index) {
                 22,
                 23,
                 26,
-                // 25,
+                25,
                 31,
                 44,
                 48,
@@ -199,7 +346,6 @@ function getImageURI(index) {
 
 const p5Instance = new p5(wheelSketch);
 
-p5Instance.setData(dataSets[currentDataSet]);
 p5Instance.onAfterSetup = function () {
     p5Instance.setVideos([
         ['videos/учит крутить рулём под phonk.mp4', 5],
@@ -281,6 +427,23 @@ for(let i = 0; i < radios.length; i++) {
         customDialog.style.display = 'none';
         p5Instance.mouseDragEnable();
 
-        p5Instance.setData(dataSets[currentDataSet]);
+        if (presets.hasPreset(currentDataSet)) {
+            if (!editedDataSets[currentDataSet]) {
+                resetEditedDataSet();
+                presets.applyDefaults(currentDataSet);
+            }
+
+            p5Instance.setData(editedDataToArray());
+            editButton.removeAttribute('disabled');
+        }
+        else {
+            p5Instance.setData(dataSets[currentDataSet]);
+            editButton.setAttribute('disabled', 'disabled');
+        }
     });
+
+    // Выбираем начальный вариант при загрузке страницы
+    if (radios[i].hasAttribute('checked')) {
+        radios[i].dispatchEvent(new Event('click'));
+    }
 }
